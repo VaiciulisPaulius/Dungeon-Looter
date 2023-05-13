67ef44887e61695038bf73eb2a4e9a8e039ebdb4
ErikasRamaneckas <erikasramaneckas@gmail.com>
2023-05-13 13:48:01 +0300

    Added spike trap

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -43,6 +43,7 @@ public class Player : MonoBehaviour
     {
         health -= dmg;
         ClampHealth();
+        Debug.Log("Player took damage: " + dmg);
     }
 
     public void AddHealth()
---
+++ Assets/SpikeTrap.cs
@@ -0,0 +1,50 @@
+//SpikeTrap.cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    private Animator anim;
+
+    private bool isWorking;
+    [SerializeField] private float cooldown;
+    [SerializeField] private float cooldownTimer;
+    [SerializeField] private float damage; // added variable for damage inflicted on player
+
+    [SerializeField] private Player player; // added reference to player object
+
+    private float damageTimer; // added timer for damage intervals
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = FindObjectOfType<Player>(); // find player object in scene
+    }
+
+    private void Update()
+    {
+        cooldownTimer -= Time.deltaTime; // fixed error - should be subtracting deltaTime
+
+        if (cooldownTimer < 0)
+        {
+            isWorking = !isWorking;
+            cooldownTimer = cooldown;
+        }
+
+        anim.SetBool("isWorking", isWorking);
+
+        if (isWorking && Vector3.Distance(transform.position, player.transform.position) < 1.0f)
+        {
+            if (damageTimer <= 0) // check if damage timer has expired
+            {
+                player.TakeDamage(damage);
+                damageTimer = 1.0f; // reset damage timer
+            }
+            else
+            {
+                damageTimer -= Time.deltaTime; // decrement damage timer
+            }
+        }
+    }
+}
