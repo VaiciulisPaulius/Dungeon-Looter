078e1a6e5ee9c2d6d5ebfd2af80cc465117510b1
maxjke <m.jako030@gmail.com>
2023-05-15 14:43:33 +0300

    Added simple inventory UI

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Inventory : MonoBehaviour 
 {
+    public static event Action<List<InventoryItem>> OnInventoryChange;
+
     public List<InventoryItem> inventory = new List<InventoryItem>();
     private Dictionary<ItemData, InventoryItem> itemDictionary = new Dictionary<ItemData, InventoryItem>();
 
@@ -22,12 +25,14 @@ public class Inventory : MonoBehaviour
        if(itemDictionary.TryGetValue(itemData, out InventoryItem item))
         {
             item.AddToStack();
+            OnInventoryChange?.Invoke(inventory);
         }
         else
         {
             InventoryItem newItem = new InventoryItem(itemData);
             inventory.Add(newItem);
             itemDictionary.Add(itemData, newItem);
+            OnInventoryChange?.Invoke(inventory);
         }
     }
 
@@ -41,6 +46,7 @@ public class Inventory : MonoBehaviour
                 inventory.Remove(item);
                 itemDictionary.Remove(itemData);
             }
+            OnInventoryChange?.Invoke(inventory);
         }
     }
 }
